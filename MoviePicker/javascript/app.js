a55fe0dc61f375aaabcfe2ec82f4464147f1503d
Jesse Chen <811337@gmail.com>
2021-10-21 12:41:36 -0500

    Change genres

--- MoviePicker/javascript/app.js
+++ MoviePicker/javascript/app.js
@@ -20,24 +20,24 @@ $(".genreRadio").click(function() {
 });
 
 /* API genre codes
-    action: 28,
-    adventure: 12,
+    action/adventure: 10759,
     animation: 16,
     comedy: 35,
     crime: 80,
     documentary: 99,
     drama: 18,
     family: 10751,
-    fantasy: 14,
-    history: 36,
-    horror: 27,
-    music: 10402,
+    kids: 10762,
     mystery: 9648,
-    romance: 10749,
-    sciFi: 878,
-    thriller: 53,
-    war: 10752,
-    western: 37
+    reality: 10764,
+    sci-fi/fantasy: 10765,
+    soap: 10766,
+    talk: 10767,
+    war/politics: 10768,
+    western: 37,
+    thriller: 53*,
+    war: 10752*,
+    western: 37*
 */
 
 $("#submitBtn").on("click", function() {
