4719be600713dae5af36678bdd685a147a57b6a3
Jesse Chen <811337@gmail.com>
2021-10-22 17:46:28 -0500

    delete medium functions

--- MoviePicker/javascript/app.js
+++ MoviePicker/javascript/app.js
@@ -9,13 +9,6 @@ let averageVote;
 let releaseDate;
 let newChoose;
 
-/*
-clicking a medium button will save "movie" or "TV" into the medium variable
-$(".mediumRadio").click(function() {
-  medium = this.value;
-});
-*/
-
 // clicking a genre button will save a genre into the genrePick variable
 $(".genreRadio").click(function() {
   genrePick = this.value;
